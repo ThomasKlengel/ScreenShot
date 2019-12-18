b6c397468fd98ad40f77d114c8e0adeda68c7d06
Thomas Klengel <thomas.klengel@xenon-automation.com>
2019-12-18 13:02:50 +0100

    * initial commit

---
+++ WpfApplication1/ScreenShooterMain.xaml.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Windows;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Runtime.InteropServices;
+
+namespace ScreenShooter
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class ScreenShooterMain : Window
+    {
+        int number = 0;
+        const double diff = 8; // fest abweichung vom sollwert der Position
+
+        private System.Windows.Point scsh_Start = new System.Windows.Point(-100, -100);
+        private System.Windows.Point scsh_Ende = new System.Windows.Point(-100, -100);
+        private System.Windows.Point OCR_Start = new System.Windows.Point(-100,-100);
+        private System.Windows.Point OCR_Ende = new System.Windows.Point(-100, -100);
+        private WindowState prevWs;
+        private string folder = @"C:\Users\Public\Pictures\Sample Pictures\ScreenShooter";        
+
+        public ScreenShooterMain()
+        {
+            InitializeComponent();           
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Hotkeys erstellen
+            hWnd = new WindowInteropHelper(this).Handle;
+            HookWndProc(this);
+            RegisterHotKey(hWnd, 0x0312, (int)Modifiers.Ctrl, (int)Keys.P);
+            RegisterHotKey(hWnd, 0x0312, (int)Modifiers.Ctrl, (int)Keys.I);
+            RegisterHotKey(hWnd, 0x0312, (int)Modifiers.Ctrl, (int)Keys.O);
+        }
+
+        private void B_Folder_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.ShowNewFolderButton = true;
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                folder = fbd.SelectedPath;
+        }
+
+        #region screenshot
+        private void b_Screenshot_Click(object sender, RoutedEventArgs e)
+        {
+            double prevLeft = Left;
+            double prevTop = Top;
+            prevWs = this.WindowState;
+            Left = -500;
+            Top = -500;
+
+            takeScreenShot();
+
+            Left = prevLeft;
+            Top = prevTop;
+            this.WindowState = prevWs;
+        }
+        private void takeScreenShot()
+        {
+            int Width = (int)( Math.Abs(OCR_Start.X - OCR_Ende.X) );
+            int Height = (int)( Math.Abs(OCR_Start.Y - OCR_Ende.Y) );
+            string fileOCR = string.Empty;
+            string text = string.Empty;
+            if (Height > 0 && Width > 0)
+            {
+                using (Bitmap bmpOCR = new Bitmap(Width, Height))
+                {
+                    using (Graphics g = Graphics.FromImage(bmpOCR))
+                    {
+                        g.CopyFromScreen((int)OCR_Start.X, (int)OCR_Start.Y, 0, 0, bmpOCR.Size);
+                        Directory.CreateDirectory(folder);
+                        fileOCR = folder + @"\OCR_Region.JPG";
+                        bmpOCR.Save(fileOCR, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        text = doOCR(fileOCR);
+                    }
+                }
+            }
+
+            if (text != string.Empty)
+            {
+                text = text.Replace("\r\n", "").Replace(@"\", "-").Replace("/", "-");
+                File.Delete(fileOCR);
+            }
+            else
+            {
+                text = number.ToString();                
+            }
+
+            Width = (int)( Math.Abs(scsh_Start.X - scsh_Ende.X) );
+            Height = (int)( Math.Abs(scsh_Start.Y - scsh_Ende.Y) );
+
+            if (Height>0 && Width>0)
+            {                
+                using (Bitmap bmpImage = new Bitmap(Width, Height))
+                {
+                    using (Graphics g = Graphics.FromImage(bmpImage))
+                    {
+                        g.CopyFromScreen((int)scsh_Start.X, (int)scsh_Start.Y, 0, 0, bmpImage.Size);
+                        Directory.CreateDirectory(folder);
+                        string filePath = folder + @"\" + text + ".png";
+                        int add_num = 1;
+                        if (File.Exists(filePath))
+                        {
+                            if (System.Windows.MessageBox.Show(
+                                "A file with the filename '" + filePath.Replace(folder + @"\", "") + "' already Exists. \r\nDo you want to override it?",
+                                "Override file", MessageBoxButton.YesNo, MessageBoxImage.Warning, MessageBoxResult.No) == MessageBoxResult.No)
+                            { 
+                                while (File.Exists(filePath))
+                                {
+                                    if (!filePath.Contains("_"))
+                                        filePath = filePath.Remove(filePath.Length - 4);
+                                    else
+                                        filePath = filePath.Remove(filePath.LastIndexOf("_"));
+
+                                    filePath += "_" + add_num.ToString() + ".png";
+                                    add_num++;
+                                }
+                            }                            
+                        }
+                        bmpImage.Save(filePath);
+                        number++;
+                    }
+                }
+            }
+            else
+            {
+                using (Bitmap bmpImage = new Bitmap(Convert.ToInt32(SystemParameters.VirtualScreenWidth), Convert.ToInt32(SystemParameters.VirtualScreenHeight)))
+                {
+                    using (Graphics g = Graphics.FromImage(bmpImage))
+                    {
+                        g.CopyFromScreen(0, 0, 0, 0, bmpImage.Size);
+                        Directory.CreateDirectory(folder);
+                        string filePath = folder + @"\" + text + ".png";
+                        int add_num = 1;
+                        if (File.Exists(filePath))
+                        {
+                            if (System.Windows.MessageBox.Show(
+                                "A file with the filename '" + filePath.Replace(folder + @"\", "") + "' already Exists. \r\nDo you want to override it?",
+                                "Override file", MessageBoxButton.YesNo, MessageBoxImage.Warning, MessageBoxResult.No) == MessageBoxResult.No)
+                            {
+                                while (File.Exists(filePath))
+                                {
+                                    if (!filePath.Contains("_"))
+                                        filePath = filePath.Remove(filePath.Length - 4);
+                                    else
+                                        filePath = filePath.Remove(filePath.LastIndexOf("_"));
+
+                                    filePath += "_" + add_num.ToString() + ".png";
+                                    add_num++;
+                                }
+                            }
+                        }
+                        bmpImage.Save(filePath);
+                        number++;
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Check for Images
+        /// read text from these images.
+        /// save text from each image in text file automaticly.
+        /// handle problems with images
+        /// </summary>
+        /// <param name="directoryPath">Set Directory Path to check for Images in it</param>
+        public string doOCR(string file)
+        {
+            try
+            {
+                //OCR Operations ... 
+                MODI.Document md = new MODI.Document();
+                md.Create(file);
+                md.OCR(MODI.MiLANGUAGES.miLANG_ENGLISH, true, true);
+                MODI.Image image = (MODI.Image)md.Images[0];
+
+                return image.Layout.Text;
+            }
+            catch (Exception exc)
+            {
+                string msgTxt;
+                if (exc.Message == "OCR running error")
+                    msgTxt = "Es konnten keine Buchstaben erkannt werden. \r\nDas Bild wird unter einer laufenden Nummer gespeichert.";
+                else msgTxt = exc.Message;
+                //uncomment the below code to see the expected errors
+                System.Windows.MessageBox.Show(msgTxt, "OCR Exception");
+                return string.Empty;
+            }
+
+        } 
+        #endregion
+
+        #region Auswahlfenster
+        private void b_SelectRegion_Click(object sender, RoutedEventArgs e)
+        {
+            prevWs = WindowState;
+            this.WindowState = WindowState.Minimized;
+            double scale = getDPIScale();
+            SelectionWindow winLocationImage = new SelectionWindow();
+            if (scsh_Start.X >= 0 || scsh_Start.Y >= 0)
+            {
+                System.Windows.Point start = new System.Windows.Point(( scsh_Start.X + diff ) / scale, ( scsh_Start.Y + diff ) / scale);
+                System.Windows.Point end = new System.Windows.Point(( scsh_Ende.X + diff ) / scale, ( scsh_Ende.Y + diff ) / scale);
+                winLocationImage = new SelectionWindow(start, end);
+            }
+            winLocationImage.RaiseCustomEvent += pointsScreenshot;
+            winLocationImage.Closed += selectionWindow_Closed;
+            winLocationImage.ShowDialog();
+        }
+        private void pointsScreenshot(object sender, customEventArgs e)
+        {
+            if (e == null || e.Points == null || e.Points.Count < 1)
+                return;
+
+            double scale = getDPIScale();            
+            scsh_Start = new System.Windows.Point(e.Points[0].X * scale - diff, e.Points[0].Y * scale - diff);
+            scsh_Ende = new System.Windows.Point(e.Points[1].X * scale - diff, e.Points[1].Y * scale - diff);
+
+        }
+
+        private void B_SelectOCR_Click(object sender, RoutedEventArgs e)
+        {
+            prevWs = WindowState;
+            this.WindowState = WindowState.Minimized;
+            double scale = getDPIScale();
+            SelectionWindow winLocationOCR = new SelectionWindow(); ;
+            if (OCR_Start.X >= 0 || OCR_Start.Y >= 0)            
+            {
+                System.Windows.Point start = new System.Windows.Point(( OCR_Start.X + diff ) / scale, ( OCR_Start.Y + diff ) / scale);
+                System.Windows.Point end = new System.Windows.Point(( OCR_Ende.X + diff ) / scale, ( OCR_Ende.Y + diff ) / scale);
+                winLocationOCR = new SelectionWindow(start, end);
+            }
+            winLocationOCR.RaiseCustomEvent += pointsOCR;
+            winLocationOCR.Closed += selectionWindow_Closed;
+            winLocationOCR.ShowDialog();
+        }
+        private void pointsOCR(object sender, customEventArgs e)
+        {
+            if (e == null || e.Points == null || e.Points.Count < 1)
+                return;
+
+            double scale = getDPIScale();             
+            OCR_Start = new System.Windows.Point(e.Points[0].X * scale - diff, e.Points[0].Y * scale - diff);
+            OCR_Ende = new System.Windows.Point(e.Points[1].X * scale - diff, e.Points[1].Y * scale - diff);
+        }
+
+        /// <summary>
+        /// gets the device pixels for scaling factor dpi/dip
+        /// </summary>
+        /// <returns>the scaling factor</returns>
+        private double getDPIScale()
+        {
+            PresentationSource source = PresentationSource.FromVisual(this);
+
+            if (source != null)
+                return source.CompositionTarget.TransformToDevice.M11;
+
+            return 0;
+        }
+        private void selectionWindow_Closed(object sender, EventArgs e)
+        {
+            this.WindowState = prevWs;
+        } 
+        #endregion
+
+        #region Hotkeys
+
+        IntPtr hWnd;
+
+        [DllImport("user32.dll")]
+        internal static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+
+        [DllImport("user32.dll")]
+        internal static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        //pass your WPF Window object to this method (Window derives from Visual)
+        private void HookWndProc(Visual window)
+        {
+            var source = PresentationSource.FromVisual(window) as HwndSource;
+            if (source == null) throw new Exception("Could not create hWnd source from window.");
+            source.AddHook(WndProc);
+        }
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg == 0x0312)
+            {
+                var lpInt = (int)lParam;
+                Keys key = (Keys)( ( lpInt >> 16 ) & 0xFFFF );
+                Modifiers modifier = (Modifiers)( lpInt & 0xFFFF );
+
+                switch (key)
+                {
+                    case Keys.I:
+                        b_SelectRegion_Click(this, new RoutedEventArgs());
+                        break;
+                    case Keys.P:
+                        b_Screenshot_Click(this, new RoutedEventArgs());
+                        break;
+                    case Keys.O:
+                        B_SelectOCR_Click(this, new RoutedEventArgs());
+                        break;
+                }
+            }
+            return IntPtr.Zero;
+        }
+
+        private enum Modifiers
+        {
+            NoMod = 0x0000,
+            Alt = 0x0001,
+            Ctrl = 0x0002,
+            Shift = 0x0004,
+            Win = 0x0008
+        } 
+        #endregion
+
+    }
+
+}
---
+++ WpfApplication1/SelectionWindow.xaml.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Threading;
+
+
+namespace ScreenShooter
+{
+    /// <summary>
+    /// Interaktionslogik für Window1.xaml
+    /// </summary>
+    ///  
+    public partial class SelectionWindow : Window
+    {
+        public event EventHandler<customEventArgs> RaiseCustomEvent;
+        private Point p_Start = new Point(-100,-100);
+        private Point p_End = new Point(-100,-100);
+        private Rectangle saveRect = null;
+        private MoveType move = MoveType.Draw;
+        private bool top = false;
+        private bool left = false;
+
+
+        public SelectionWindow()
+        {
+            InitializeComponent();            
+            WindowState = WindowState.Maximized;
+        }
+
+        public SelectionWindow(Point a, Point b)
+        {
+            InitializeComponent();
+            WindowState = WindowState.Maximized;
+
+            Rectangle r = new Rectangle();
+            r.Stroke = new SolidColorBrush(Colors.Red);
+            r.Opacity = 1;
+            r.Height = Math.Abs(b.Y-a.Y);
+            r.Width = Math.Abs(b.X - a.X);
+            r.VerticalAlignment = VerticalAlignment.Top;
+            r.HorizontalAlignment = HorizontalAlignment.Left;
+            r.Margin = new Thickness(a.X, a.Y, 0, 0);
+            this.mainGrid.Children.Add(r);
+            saveRect = r;
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            
+            p_Start = e.GetPosition(null);
+
+            double xAct = e.GetPosition(null).X;
+            double yAct = e.GetPosition(null).Y;
+
+            if (saveRect != null)
+            {
+                double rectLeft = saveRect.Margin.Left;
+                double rectTop = saveRect.Margin.Top;
+                double rectRight = saveRect.Margin.Left + saveRect.Width;
+                double rectBottom = saveRect.Margin.Top + saveRect.Height;
+
+                if (between(xAct, rectLeft + 3, rectRight - 3) &&
+                    between(yAct, rectTop + 3, rectBottom - 3))
+                    move = MoveType.Drag;
+
+                else if (( between(xAct, rectLeft - 3, rectLeft) || between(xAct, rectRight - 3, rectRight) ) &&
+                    between(yAct, rectTop, rectBottom))
+                {
+                    move = MoveType.ResizeWidth;
+                    left = between(xAct, rectLeft - 3, rectLeft + 3);
+                }
+                else if (( between(yAct, rectTop - 3, rectTop) || between(yAct, rectBottom - 3, rectBottom) ) &&
+                    between(xAct, rectLeft, rectRight))
+                {
+                    move = MoveType.ResizeHeight;
+                    top = between(yAct, rectTop - 3, rectTop + 3);
+                }
+                else
+                    move = MoveType.Draw;
+            }
+            else
+                move = MoveType.Draw;
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (CM.IsVisible)
+            {                
+                return;
+            }
+
+            double xAct = e.GetPosition(null).X;
+            double yAct = e.GetPosition(null).Y;
+
+            #region select Cursor
+            if (e.LeftButton != MouseButtonState.Pressed)
+            {
+                if (saveRect != null)
+                {
+                    double rectLeft = saveRect.Margin.Left;
+                    double rectTop = saveRect.Margin.Top;
+                    double rectRight = saveRect.Margin.Left + saveRect.Width;
+                    double rectBottom = saveRect.Margin.Top + saveRect.Height;
+
+                    if (between(xAct, rectLeft + 3, rectRight - 3) &&
+                        between(yAct, rectTop + 3, rectBottom - 3))
+                        Cursor = Cursors.Hand;
+
+                    else if (( between(xAct, rectLeft - 3, rectLeft) || between(xAct, rectRight - 3, rectRight) ) &&
+                        between(yAct, rectTop, rectBottom))
+                        Cursor = Cursors.SizeWE;
+                    else if (( between(yAct, rectTop - 3, rectTop) || between(yAct, rectBottom - 3, rectBottom) ) &&
+                        between(xAct, rectLeft, rectRight))
+                        Cursor = Cursors.SizeNS;
+
+                    else Cursor = Cursors.Pen;
+                }
+                else
+                    Cursor = Cursors.Pen;
+                return;
+            }
+            else
+            {
+                switch (move)
+                {
+                    case MoveType.Drag:
+                        Cursor = Cursors.Hand;
+                        break;
+                    case MoveType.Draw:
+                        Cursor = Cursors.Pen;
+                        break;
+                    case MoveType.ResizeHeight:
+                        Cursor = Cursors.SizeNS;
+                        break;
+                    case MoveType.ResizeWidth:
+                        Cursor = Cursors.SizeWE;
+                        break;
+                }
+            }
+            #endregion
+
+            #region scale, drag, draw the rectangle
+            if (mainGrid.Children.Count > 0)
+                this.mainGrid.Children.RemoveAt(0);
+            Rectangle r = new Rectangle();
+            p_End = e.GetPosition(null);
+            switch (move)
+            {   // Rechteck Zeichnen
+                case MoveType.Draw:
+                    r.Stroke = new SolidColorBrush(Colors.Red);
+                    r.Opacity = 1;
+                    r.Height = Math.Abs(p_End.Y - p_Start.Y);
+                    r.Width = Math.Abs(p_End.X - p_Start.X);
+                    double t_left = p_End.X > p_Start.X ? p_Start.X : p_End.X;
+                    double t_top = p_End.Y > p_Start.Y ? p_Start.Y : p_End.Y;
+                    r.VerticalAlignment = VerticalAlignment.Top;
+                    r.HorizontalAlignment = HorizontalAlignment.Left;
+                    r.Margin = new Thickness(t_left, t_top, 0, 0);
+                    this.mainGrid.Children.Add(r);
+                    saveRect = r;
+                    break;
+                // Rechteck verschieben
+                case MoveType.Drag:
+                    r = saveRect;
+                    double moveHorizontal = p_Start.X - p_End.X;
+                    double moveVertical = p_Start.Y - p_End.Y;
+                    if (r.Margin.Left - moveHorizontal < 0)
+                        moveHorizontal = r.Margin.Left;
+                    if (r.Margin.Top - moveVertical < 0)
+                        moveVertical = r.Margin.Top;
+                    r.Margin = new Thickness(r.Margin.Left - moveHorizontal, r.Margin.Top - moveVertical, 0, 0);
+                    this.mainGrid.Children.Add(r);
+                    saveRect = r;
+                    p_Start = p_End;
+                    break;
+                // Rechteckhöhe skalieren
+                case MoveType.ResizeHeight:
+                    r = saveRect;
+                    double resizeY = p_Start.Y - p_End.Y;
+                    try
+                    {
+                        if (top)
+                        {
+                            if (r.Height - resizeY > 0)
+                            {
+                                r.Margin = new Thickness(r.Margin.Left, r.Margin.Top - resizeY, 0, 0);
+                                r.Height += resizeY;
+                            }
+                        }
+                        else
+                        {
+                            if (r.Height - resizeY > 0)
+                                r.Height -= resizeY;
+                        }
+                    }
+                    catch (Exception) { }
+                    this.mainGrid.Children.Add(r);
+                    saveRect = r;
+                    p_Start = p_End;
+                    break;
+                // Rechteckbreite skalieren
+                case MoveType.ResizeWidth:
+                    r = saveRect;
+                    double resizeX = p_Start.X - p_End.X;
+                    try
+                    {
+                        if (left)
+                        {
+                            if (r.Width - resizeX > 0)
+                            {
+                                r.Margin = new Thickness(r.Margin.Left - resizeX, r.Margin.Top, 0, 0);
+                                r.Width += resizeX;
+                            }
+                        }
+                        else
+                        {
+                            if (r.Width - resizeX > 0)
+                                r.Width -= resizeX;
+                        }
+                    }
+                    catch (Exception) { }
+                    this.mainGrid.Children.Add(r);
+                    saveRect = r;
+                    p_Start = p_End;
+                    break;
+            } 
+            #endregion
+
+            // for cpu utilisation reasons
+            Thread.Sleep(10);
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Return:
+                    CM_End_Click(this, new RoutedEventArgs());
+                    break;
+                case Key.Escape:
+                    CM_Abort_Click(this, new RoutedEventArgs());
+                    break;
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Topmost = true;
+            Activate();
+            Focus();
+        }
+
+        /// <summary>
+        /// gets the upper left and lower right point of the last drawn selection rectangle
+        /// </summary>
+        /// <param name="abort">returns an emtpy list of points if true</param>
+        /// <returns></returns>
+        public List<Point> getPoints(bool abort)
+        {
+            List<Point> p = new List<Point>();
+            if (abort)
+                return p;
+            if (mainGrid.Children.Count > 0)
+            {
+                p.Add(new Point(saveRect.Margin.Left, saveRect.Margin.Top));
+                p.Add(new Point(saveRect.Width+ saveRect.Margin.Left, saveRect.Height+ saveRect.Margin.Top));
+                return p;
+            }
+
+            p.Clear();
+            p.Add(new Point(-100, -100));
+            p.Add(new Point(-100, -100));
+            return p;
+        }
+
+        /// <summary>
+        /// if a value is between the boundaries
+        /// </summary>
+        /// <param name="actual">the value to be tested</param>
+        /// <param name="min">lower boundary</param>
+        /// <param name="max">upper boundary</param>
+        /// <returns>true if the value is between the boundaries</returns>
+        private bool between(double actual, double min, double max)
+        {
+            if (( actual < min ) || ( actual > max ))
+                return false;
+            else return true;
+        }
+
+        private enum MoveType
+        {
+            Draw,
+            Drag,
+            ResizeHeight,
+            ResizeWidth
+        }
+
+        #region Context Menu
+        private void CM_End_Click(object sender, RoutedEventArgs e)
+        {
+            RaiseCustomEvent(this, new customEventArgs(getPoints(false)));
+            this.Close();
+        }
+
+        private void CM_Abort_Click(object sender, RoutedEventArgs e)
+        {
+            RaiseCustomEvent(this, new customEventArgs(getPoints(true)));
+            this.Close();
+        }
+
+        private void CM_Reset_Click(object sender, RoutedEventArgs e)
+        {
+            mainGrid.Children.Clear();
+        } 
+        #endregion
+
+    }
+
+    public class customEventArgs : EventArgs
+    {
+        private List<Point> pts;
+        public customEventArgs(List<Point> Points)
+        {
+            pts = Points;
+        }
+
+        public List<Point> Points
+        {
+            get { return pts; }
+        }
+    }
+}
